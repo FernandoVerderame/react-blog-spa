a3afe52a4e2b97a284f66ed8ded51634cf50aa56
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-23 12:15:58 +0200

    added posts route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DefaultLayout from "./pages/Layout/DefaultLayout.jsx";
 import HomePage from "./pages/HomePage/HomePage.jsx";
+import Posts from "./pages/Posts/Posts.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<DefaultLayout />}>
-          <Route path="/" index element={<HomePage />} />
+          <Route index element={<HomePage />} />
+          <Route path="posts" element={<Posts />} />
         </Route>
       </Routes>
     </BrowserRouter>
