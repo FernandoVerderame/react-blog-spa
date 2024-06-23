8c33d891b0bba8c30fddd347216045f5bfb8423f
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-23 12:16:19 +0200

    updated navbar links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,13 +2,13 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-secondary-subtle">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Blog</a>
+                <a className="navbar-brand" href="/">Blog</a>
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Home</a>
+                        <a className="nav-link" href="/">Home</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Posts</a>
+                        <a className="nav-link" href="posts">Posts</a>
                     </li>
                 </ul>
             </div>
