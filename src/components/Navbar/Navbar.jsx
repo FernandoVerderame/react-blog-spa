2ae2e434ee82e395b69f9c6c2b079457c70885c9
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-23 12:21:51 +0200

    added navlinks

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,16 @@
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-secondary-subtle">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">Blog</a>
+                <NavLink className="navbar-brand" to={'/'}>Blog</NavLink>
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                        <a className="nav-link" href="/">Home</a>
+                        <NavLink className="nav-link" to={'/'}>Home</NavLink>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="posts">Posts</a>
+                        <NavLink className="nav-link" to={'/posts'}>Posts</NavLink>
                     </li>
                 </ul>
             </div>
