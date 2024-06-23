7ce394d2bdb425ae12415308c9cb0c78f6d7d3ae
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-23 12:06:34 +0200

    added app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DefaultLayout from "./pages/Layout/DefaultLayout.jsx";
+import HomePage from "./pages/HomePage/HomePage.jsx";
+
 function App() {
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<DefaultLayout />}>
+          <Route path="/" index element={<HomePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
